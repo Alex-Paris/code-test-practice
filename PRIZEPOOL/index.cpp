7d12775d21c5edfa0f465babe23dbc5b27afc339
Alex Paris <alex_paris28@hotmail.com>
2022-05-29 00:52:14 -0300

    chore: PRIZEPOOL

---
+++ PRIZEPOOL/index.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int t;
+  cin >> t;
+
+  for (int tc = 0; tc < t; tc++)
+  {
+    int x, y;
+    cin >> x >> y;
+
+    cout << 10 * x + 90 * y << "\n";
+  }
+  return 0;
+}
